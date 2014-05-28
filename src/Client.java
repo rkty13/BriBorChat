1ffa98685ff6a1845464e8cf978a85267700326b
ekongg <elk2son@yahoo.com>
2014-05-28 12:22:38 -0400

    sdgdhf

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,11 @@
 
 public class Client {
-
+    
+    public Client(){
+        
+    }
+    
+    public static void main(String[]args){
+        
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -3,5 +3,12 @@ import java.net.ServerSocket;
 public class Server {
 	private ServerSocket s;
 	
+	public Server(){
+	    
+	}
+	
+	public static void main(String[]args){
+	    
+	}
 
 }
