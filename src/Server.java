656286ced02bdbc36b3f0674c56c0d910354ef58
TheDoctor0007 <rhyan42@gmail.com>
2014-05-30 15:31:27 -0400

    Revert "fdsaf"
    This reverts commit d92d9791322777c1fbfe3891ea4b83bb9af64866.

--- src/Server.java
+++ src/Server.java
@@ -10,7 +10,6 @@ public class Server {
 	public static void main(String[]args){
 	    try {
 			ServerSocket s = new ServerSocket(4);
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
