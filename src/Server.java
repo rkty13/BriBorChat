d637501336fa849e6a5e9e1db595483694e009c3
TheDoctor0007 <rhyan42@gmail.com>
2014-05-30 10:11:19 -0400

    Added error catching

--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,19 @@
+import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Server {
-	private ServerSocket s;
 	
 	public Server(){
 	    
 	}
 	
 	public static void main(String[]args){
-	    
+	    try {
+			ServerSocket s = new ServerSocket(4);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
