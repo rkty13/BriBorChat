917dd3d217176773b06038bb8c5b81fb1ab16842
TheDoctor0007 <rhyan42@gmail.com>
2014-06-02 09:21:33 -0400

    unimportant changes so there are no conflicts

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 	
@@ -9,7 +10,9 @@ public class Server {
 	
 	public static void main(String[]args){
 	    try {
-			ServerSocket s = new ServerSocket(4);
+			ServerSocket server = new ServerSocket(4);
+			Socket client = server.accept();
+		    
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
