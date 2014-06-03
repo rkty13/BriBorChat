ce568acfd0862391376a3ca40498836bcf483e87
ekongg <elk2son@yahoo.com>
2014-06-03 08:33:59 -0400

    uis

--- src/ChatGUI.java
+++ src/ChatGUI.java
@@ -12,6 +12,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRootPane;
 import javax.swing.JScrollPane;
@@ -50,13 +51,24 @@ public class ChatGUI extends JFrame{
         });
         
         aboutButton.setMnemonic(KeyEvent.VK_A);
-        
+        aboutButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(final ActionEvent ae){
+                ImageIcon icon = new ImageIcon("resources/HydraIcon.jpg");
+                JOptionPane.showMessageDialog(null, "   The BBChat Project was created by Sophomores: Eric Kong, Parth Mistry, and Robert Kim.\n"
+                        + "This program was designed to be an instant messenger program using Java GUI's and Java ServerSockets.\n"
+                        + "Robert Kim has contributed the least to this Project.", "About", JOptionPane.INFORMATION_MESSAGE, icon);
+            }
+        });
         
         JRootPane rootPane = getRootPane();
         rootPane.setDefaultButton(sendButton);
         
         final JPanel chatPanel = new JPanel();
         chatPanel.add(scrollPane);
+        
+        final JPanel aboutPanel = new JPanel();
+        aboutPanel.add(aboutButton);
         
         final JPanel textPanel = new JPanel();
         textPanel.add(messageLabel);
@@ -65,8 +77,10 @@ public class ChatGUI extends JFrame{
    
         final Container mainPanel = getContentPane();
         mainPanel.setLayout(new BorderLayout());
-        mainPanel.add(chatPanel, BorderLayout.CENTER);
-        mainPanel.add(textPanel, BorderLayout.SOUTH);
+        mainPanel.add(chatPanel, BorderLayout.NORTH);
+        mainPanel.add(textPanel, BorderLayout.CENTER);
+        mainPanel.add(aboutPanel, BorderLayout.SOUTH);
+        
         
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
