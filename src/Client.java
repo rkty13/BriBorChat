1cc9d781e050445be2b3cb23367e1f10d308b43c
Robert Kim <rkty13@gmail.com>
2014-06-04 12:02:16 -0400

    added cli args

--- src/Client.java
+++ src/Client.java
@@ -6,30 +6,35 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		try {
-			Socket echoSocket = new Socket("67.81.222.76", 18304);
-			System.out.println("Connection successful!");
-			System.out.println("write something");
-			PrintWriter out = new PrintWriter(echoSocket.getOutputStream(),
-					true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					echoSocket.getInputStream()));
-			BufferedReader stdIn = new BufferedReader(new InputStreamReader(
-					System.in));
-			String userInput;
-			while ((userInput = stdIn.readLine()) != null) {
-				out.println(userInput);
-				System.out.println("echo: " + in.readLine());
-			}
-		} catch (UnknownHostException e) {
-			System.err.println("Unknown Host");
-			System.exit(1);
-		} catch (IOException e) {
-			System.err
-					.println("Couldn't get I/O for the connection to BBServer");
-			System.exit(1);
-		}
-	}
+        if (args.length != 2) {
+            System.err.println("Usage: java Client <host> <port>");
+            System.exit(1);
+        }
+
+        try {
+            Socket echoSocket = new Socket(args[0], Integer.parseInt(args[1]));
+            System.out.println("Connection successful!");
+            System.out.println("write something");
+            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(),
+                    true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    echoSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(
+                    System.in));
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Unknown Host");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err
+                    .println("Couldn't get I/O for the connection to BBServer");
+            System.exit(1);
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -10,79 +10,85 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class Server {
 
-	public static ArrayList<ClientThread> users = new ArrayList<ClientThread>();
-	public static LinkedBlockingQueue<String> messages = new LinkedBlockingQueue<String>();
+    public static ArrayList<ClientThread> users = new ArrayList<ClientThread>();
+    public static LinkedBlockingQueue<String> messages = new LinkedBlockingQueue<String>();
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		try {
+        if (args.length != 1) {
+            System.err.println("Usage: java Server <port>");
+            System.exit(1);
+        }
 
-			ServerSocket serverSocket = new ServerSocket(18304);
+        try {
 
-			while (true) {
-				Socket clientSocket = serverSocket.accept();
- 
-				InetAddress inetAddress = clientSocket.getInetAddress();
+            ServerSocket serverSocket = new ServerSocket(
+                    Integer.parseInt(args[0]));
 
-				System.out.println("Client: " + inetAddress.getHostName()
-						+ " | " + inetAddress.getHostAddress() + " connected.");
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
 
-				ClientThread thread = new ClientThread(clientSocket, "rkty13");
-				users.add(thread);
-				new Thread(thread).start();
+                InetAddress inetAddress = clientSocket.getInetAddress();
 
-				while (!messages.isEmpty()) {
-					String message = messages.remove();
-					for (ClientThread user : users) {
-						user.sendMessage(message);
-					}
+                System.out.println("Client: " + inetAddress.getHostName()
+                        + " | " + inetAddress.getHostAddress() + " connected.");
 
-				}
-			}
-			/*
-			 * PrintWriter out = new PrintWriter(
-			 * clientSocket.getOutputStream(), true); BufferedReader in = new
-			 * BufferedReader(new InputStreamReader(
-			 * clientSocket.getInputStream())); String inputLine; while
-			 * ((inputLine = in.readLine()) != null) { out.println(inputLine); }
-			 */
-		} catch (IOException e) {
-			System.out
-					.println("Exception caught when trying to listen on port "
-							+ 18304 + " or listening for a connection");
-			System.out.println(e.getMessage());
-		}
-	}
+                ClientThread thread = new ClientThread(clientSocket, "rkty13");
+                users.add(thread);
+                new Thread(thread).start();
+
+                while (!messages.isEmpty()) {
+                    String message = messages.remove();
+                    for (ClientThread user : users) {
+                        user.sendMessage(message);
+                    }
+
+                }
+            }
+            /*
+             * PrintWriter out = new PrintWriter(
+             * clientSocket.getOutputStream(), true); BufferedReader in = new
+             * BufferedReader(new InputStreamReader(
+             * clientSocket.getInputStream())); String inputLine; while
+             * ((inputLine = in.readLine()) != null) { out.println(inputLine); }
+             */
+        } catch (IOException e) {
+            System.out
+                    .println("Exception caught when trying to listen on port "
+                            + 18304 + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
 class ClientThread implements Runnable {
 
-	private Socket socket;
-	private String username;
-	private BufferedReader in;
-	private PrintWriter out;
+    private Socket socket;
+    private String username;
+    private BufferedReader in;
+    private PrintWriter out;
 
-	public ClientThread(Socket socket, String username) throws IOException {
-		this.socket = socket;
-		this.username = username;
-		out = new PrintWriter(this.socket.getOutputStream(), true);
-		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    public ClientThread(Socket socket, String username) throws IOException {
+        this.socket = socket;
+        this.username = username;
+        out = new PrintWriter(this.socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-	}
+    }
 
-	@Override
-	public void run() {
-		try {
-			String message;
-			while ((message = in.readLine()) != null) {
-				Server.messages.add(username + ": " + message);
-			}
-		} catch (IOException e) {
-			System.err.println(e);
-		}
-	}
+    @Override
+    public void run() {
+        try {
+            String message;
+            while ((message = in.readLine()) != null) {
+                Server.messages.add(username + ": " + message);
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
 
-	public void sendMessage(String message) {
-		out.println(message);
-	}
+    public void sendMessage(String message) {
+        out.println(message);
+    }
 }
