29541cc9e7dd54997b4ee8351bbce480d1fd767b
Robert Kim <rkty13@gmail.com>
2014-06-04 01:16:41 -0400

    someone keeps committing stuff that breaks the sync

--- src/Client.java
+++ src/Client.java
@@ -9,18 +9,20 @@ public class Client {
 	public static void main(String[] args) throws IOException {
 
 		try {
-			Socket echoSocket = new Socket("67.81.222.76", 8000);
+			Socket echoSocket = new Socket("67.81.222.76", 18304);
 			System.out.println("Connection successful!");
-			/*
-			 * System.out.println("write something"); PrintWriter out = new
-			 * PrintWriter(echoSocket.getOutputStream(), true); BufferedReader
-			 * in = new BufferedReader(new InputStreamReader(
-			 * echoSocket.getInputStream())); BufferedReader stdIn = new
-			 * BufferedReader(new InputStreamReader( System.in)); String
-			 * userInput; while ((userInput = stdIn.readLine()) != null) {
-			 * out.println(userInput); System.out.println("echo: " +
-			 * in.readLine()); }
-			 */
+			System.out.println("write something");
+			PrintWriter out = new PrintWriter(echoSocket.getOutputStream(),
+					true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					echoSocket.getInputStream()));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(
+					System.in));
+			String userInput;
+			while ((userInput = stdIn.readLine()) != null) {
+				out.println(userInput);
+				System.out.println("echo: " + in.readLine());
+			}
 		} catch (UnknownHostException e) {
 			System.err.println("Unknown Host");
 			System.exit(1);
--- src/Server.java
+++ src/Server.java
@@ -5,12 +5,20 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Server {
+
+	public static ArrayList<ClientThread> users = new ArrayList<ClientThread>();
+	public static LinkedBlockingQueue<String> messages = new LinkedBlockingQueue<String>();
+
 	public static void main(String[] args) throws IOException {
 
 		try {
-			ServerSocket serverSocket = new ServerSocket(8000);
+
+			ServerSocket serverSocket = new ServerSocket(18304);
+
 			while (true) {
 				Socket clientSocket = serverSocket.accept();
 
@@ -18,6 +26,18 @@ public class Server {
 
 				System.out.println("Client: " + inetAddress.getHostName()
 						+ " | " + inetAddress.getHostAddress() + " connected.");
+
+				ClientThread thread = new ClientThread(clientSocket, "rkty13");
+				users.add(thread);
+				new Thread(thread).start();
+
+				while (!messages.isEmpty()) {
+					String message = messages.remove();
+					for (ClientThread user : users) {
+						user.sendMessage(message);
+					}
+
+				}
 			}
 			/*
 			 * PrintWriter out = new PrintWriter(
@@ -29,8 +49,40 @@ public class Server {
 		} catch (IOException e) {
 			System.out
 					.println("Exception caught when trying to listen on port "
-							+ 8000 + " or listening for a connection");
+							+ 18304 + " or listening for a connection");
 			System.out.println(e.getMessage());
 		}
+	}
+}
+
+class ClientThread implements Runnable {
+
+	private Socket socket;
+	private String username;
+	private BufferedReader in;
+	private PrintWriter out;
+
+	public ClientThread(Socket socket, String username) throws IOException {
+		this.socket = socket;
+		this.username = username;
+		out = new PrintWriter(this.socket.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+	}
+
+	@Override
+	public void run() {
+		try {
+			String message;
+			while ((message = in.readLine()) != null) {
+				Server.messages.add(username + ": " + message);
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
+	public void sendMessage(String message) {
+		out.println(message);
 	}
 }
