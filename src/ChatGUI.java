838487071cea386c3a6ca65a92310da2bb919627
TheDoctor0007 <rhyan42@gmail.com>
2014-06-05 08:35:55 -0400

    I got the Color

--- src/ChatGUI.java
+++ src/ChatGUI.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Image;
@@ -6,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -27,6 +29,9 @@ public class ChatGUI extends JFrame {
 	private static JTextArea chatBox;
 	private static JLabel n = chatRoomGUI.name;
 	private static ImageIcon hap, sad, annoy, cool, cry, exc, slp, spls, srsly, tng, wink, wond, wry;
+	private static Random rand;
+	private static Color randCol;
+	private static float r, g, b;
 
 	public ChatGUI() {
 		super(chatRoomGUI.chatRoomName);
@@ -34,7 +39,13 @@ public class ChatGUI extends JFrame {
 		chatBox = new JTextArea(20, 50);
 		sendButton = new JButton("Send");
 		aboutButton = new JButton("About");
+		rand = new Random();
 		hap = new ImageIcon("Happy.jpg");
+		
+		r = rand.nextFloat();
+		g = rand.nextFloat();
+		b = rand.nextFloat();
+		randCol = new Color(r, g, b);
 
 		JLabel messageLabel = new JLabel("Message:");
 		messageLabel.setDisplayedMnemonic('M');
@@ -101,6 +112,7 @@ public class ChatGUI extends JFrame {
 		if (message.equals("")) {
 		} else {
 			MessageBox.setText("");
+			chatBox.setForeground(randCol);
 			chatBox.append(n.getText() + ": " + message);
 			chatBox.append("\n");
 		}
