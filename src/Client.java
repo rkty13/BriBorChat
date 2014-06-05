ee87c82345fb7674900807bdf72966205578af1a
Robert Kim <rkty13@gmail.com>
2014-06-05 18:35:28 -0400

    asdf

--- src/Client.java
+++ src/Client.java
@@ -2,61 +2,66 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
 
-    private DataInputStream in;
-    private DataOutputStream out;
-    private Scanner messageInput;
+	private DataInputStream in;
+	private DataOutputStream out;
+	
+	public String username;
 
-    public Client() {
-        messageInput = new Scanner(System.in);
-        Socket socket = null;
-        try {
-            socket = new Socket("67.81.222.76", 18304);
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            Thread sending = new Thread() {
-                public void run() {
-                    try {
-                        while (true) {
-                            send();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-            sending.start();
+	public Client() {
+		Socket socket = null;
+		try {
+			socket = new Socket("67.81.222.76", 18304);
+			in = new DataInputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+			
+			/*
+			Thread sending = new Thread() {
+				public void run() {
+					try {
+						while (true) {
+							send();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			sending.start();
+			*/
 
-            Thread receiving = new Thread() {
-                public void run() {
-                    while (true) {
-                        receive();
-                    }
-                }
-            };
-            receiving.start();
-        } catch (IOException e) {
-            System.err.println("hi");
-        }
-    }
+			Thread receiving = new Thread() {
+				public void run() {
+					while (true) {
+						receive();
+					}
+				}
+			};
+			receiving.start();
+		} catch (IOException e) {
+			System.err.println("hi");
+		}
+	}
 
-    public void send() throws IOException {
-        String message = messageInput.nextLine();
-        out.writeUTF(message);
-    }
+	public void send(String message) {
+		try {
+			out.writeUTF(message);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void receive() {
-        try {
-            System.out.println(in.readUTF());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public void receive() {
+		try {
+			System.out.println(in.readUTF());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static void main(String[] args) {
-        new Client();
-    }
+	public static void main(String[] args) {
+		new Client();
+	}
 }
