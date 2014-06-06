111ae6450ed1fe9d94780ac52a861b3e1ae1b732
Robert Kim <rkty13@gmail.com>
2014-06-06 09:16:05 -0400

    merge

--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -143,5 +144,18 @@ class HandleClient implements Runnable {
     public void sendMessage(String message, String fromUsername)
             throws IOException {
         out.writeUTF(fromUsername + ": " + message);
+    }
+}
+
+@SuppressWarnings("serial")
+class Username implements Serializable {
+    public String username;
+
+    public Username(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
