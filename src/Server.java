2b63cc8283e24bed1c2bbd17f592488b2ac958ae
Robert Kim <rkty13@gmail.com>
2014-06-12 18:23:00 -0400

    asdf

--- src/Server.java
+++ src/Server.java
@@ -7,147 +7,152 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
-    public static ArrayList<HandleClient> clients;
+	public static ArrayList<HandleClient> clients;
 
-    public static void main(String[] args) {
-        new Server();
-    }
+	public static void main(String[] args) {
+		new Server();
+	}
 
-    @SuppressWarnings("resource")
-    public Server() {
-        clients = new ArrayList<HandleClient>();
+	@SuppressWarnings("resource")
+	public Server() {
+		clients = new ArrayList<HandleClient>();
 
-        // initialize ServerSocket and listen for connections
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(18304);
+		// initialize ServerSocket and listen for connections
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(18304);
 
-            int clientNum = 1;
+			int clientNum = 1;
 
-            while (true) {
-                // listen for client connection
-                Socket connection = null;
-                String username = null;
-                ObjectInputStream inputName = null;
-                try {
-                    // accept client connection
-                    connection = serverSocket.accept();
-                    inputName = new ObjectInputStream(
-                            connection.getInputStream());
+			while (true) {
+				// listen for client connection
+				Socket connection = null;
+				String username = null;
+				ObjectInputStream inputName = null;
+				try {
+					// accept client connection
+					connection = serverSocket.accept();
+					inputName = new ObjectInputStream(
+							connection.getInputStream());
 
-                    // receive user name from client
-                    username = (String) inputName.readObject();
+					// receive user name from client
+					username = (String) inputName.readObject();
 
-                    // check if user name given by client is taken
-                    new DataOutputStream(connection.getOutputStream())
-                            .writeBoolean(checkUserTaken(username));
+					// check if user name given by client is taken
+					new DataOutputStream(connection.getOutputStream())
+							.writeBoolean(checkUserTaken(username));
 
-                    System.out.println("Client #" + clientNum
-                            + " connected as '" + username + "'.");
-                } catch (IOException e) {
-                    System.err.println("Error connecting client to server.");
-                    e.printStackTrace();
-                    continue;
-                } catch (ClassNotFoundException e) {
-                    System.err.println("Error receiving username from client #"
-                            + clientNum + ".");
-                    e.printStackTrace();
-                    continue;
-                }
+					System.out.println("Client #" + clientNum
+							+ " connected as '" + username + "'.");
+					writeToAll("'" + username + "' joined the chat room.", -1,
+							"SERVER");
+				} catch (IOException e) {
+					System.err.println("Error connecting client to server.");
+					e.printStackTrace();
+					continue;
+				} catch (ClassNotFoundException e) {
+					System.err.println("Error receiving username from client #"
+							+ clientNum + ".");
+					e.printStackTrace();
+					continue;
+				}
 
-                HandleClient task = new HandleClient(connection, clientNum,
-                        username);
-                clients.add(task);
+				HandleClient task = new HandleClient(connection, clientNum,
+						username);
+				clients.add(task);
 
-                new Thread(task).start();
-                clientNum++;
+				new Thread(task).start();
+				clientNum++;
 
-            }
-        } catch (IOException e) {
-            System.err.println("Error creating new ServerSocket.");
-            e.printStackTrace();
-        }
-    }
+			}
+		} catch (IOException e) {
+			System.err.println("Error creating new ServerSocket.");
+			e.printStackTrace();
+		}
+	}
 
-    // removes client from ArrayList of connected clients
-    public static void remove(int clientNum) {
-        for (int i = 0; i < clients.size(); i++) {
-            if (clients.get(i).clientNum == clientNum) {
-                clients.remove(i);
-            }
-        }
-    }
+	// removes client from ArrayList of connected clients
+	public static void remove(int clientNum) {
+		for (int i = 0; i < clients.size(); i++) {
+			if (clients.get(i).clientNum == clientNum) {
+				clients.remove(i);
+			}
+		}
+	}
 
-    // writes messages to all clients
-    public static void writeToAll(String message, int clientNum,
-            String fromUsername) {
-        for (HandleClient client : clients) {
-            try {
-                if (!(client.clientNum == clientNum)) {
-                    client.sendMessage(message, fromUsername);
-                }
+	// writes messages to all clients
+	public static void writeToAll(String message, int clientNum,
+			String fromUsername) {
+		for (HandleClient client : clients) {
+			try {
+				if (!(client.clientNum == clientNum)) {
+					client.sendMessage(message, fromUsername);
+				}
 
-            } catch (IOException e) {
-                System.err.println("Error sending to client #"
-                        + client.clientNum);
-                continue;
-            }
-        }
-    }
+			} catch (IOException e) {
+				System.err.println("Error sending to client #"
+						+ client.clientNum);
+				continue;
+			}
+		}
+	}
 
-    // checks if user name is taken by another client, returns a boolean
-    public boolean checkUserTaken(String username) {
-        for (HandleClient client : clients) {
-            if (client.username.equalsIgnoreCase(username)) {
-                return false;
-            }
-        }
-        return true;
-    }
+	// checks if user name is taken by another client, returns a boolean
+	public boolean checkUserTaken(String username) {
+		for (HandleClient client : clients) {
+			if (client.username.equalsIgnoreCase(username)
+					|| client.username.equalsIgnoreCase("SERVER")) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
 class HandleClient implements Runnable {
-    private Socket connection;
-    public int clientNum;
-    private DataInputStream in;
-    private DataOutputStream out;
-    public String username;
+	private Socket connection;
+	public int clientNum;
+	private DataInputStream in;
+	private DataOutputStream out;
+	public String username;
 
-    public HandleClient(Socket connection, int clientNum, String username) {
-        this.connection = connection;
-        this.clientNum = clientNum;
-        this.username = username;
-    }
+	public HandleClient(Socket connection, int clientNum, String username) {
+		this.connection = connection;
+		this.clientNum = clientNum;
+		this.username = username;
+	}
 
-    @Override
-    public void run() {
-        try {
+	@Override
+	public void run() {
+		try {
 
-            in = new DataInputStream(connection.getInputStream());
-            out = new DataOutputStream(connection.getOutputStream());
+			in = new DataInputStream(connection.getInputStream());
+			out = new DataOutputStream(connection.getOutputStream());
 
-            while (true) {
-                try {
-                    String message = in.readUTF();
-                    System.out.println("Message: '" + message
-                            + "' received from " + username + ".");
-                    Server.writeToAll(message, clientNum, username);
-                } catch (Exception e) {
-                    System.err.println("Client #" + clientNum
-                            + " disconnected.");
-                    Server.remove(clientNum);
-                    break;
-                }
-            }
+			while (true) {
+				try {
+					String message = in.readUTF();
+					System.out.println("Message: '" + message
+							+ "' received from " + username + ".");
+					Server.writeToAll(message, clientNum, username);
+				} catch (Exception e) {
+					System.err.println("Client #" + clientNum
+							+ " disconnected.");
+					Server.writeToAll("'" + username + "' left the chat room.",
+							-1, "SERVER");
+					Server.remove(clientNum);
+					break;
+				}
+			}
 
-        } catch (IOException e) {
-            System.err.println("Error creating Data Input/Output Streams.");
-            e.printStackTrace();
-        }
-    }
+		} catch (IOException e) {
+			System.err.println("Error creating Data Input/Output Streams.");
+			e.printStackTrace();
+		}
+	}
 
-    public void sendMessage(String message, String fromUsername)
-            throws IOException {
-        out.writeUTF(fromUsername + ": " + message);
-    }
+	public void sendMessage(String message, String fromUsername)
+			throws IOException {
+		out.writeUTF(fromUsername + ": " + message);
+	}
 }
