cdfa8b8e3f006c98d87e15b4512e64f06656bc3b
ekongg <elk2son@yahoo.com>
2014-06-13 00:14:51 -0400

    ew

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 rm -f *.class *.jar
-javac -sourcepath src -d bin src/BriBorChat.java
-jar -cfm BriBorChat.jar MainClass resources -C bin/ .
+javac -sourcepath src -d bin src/ChatRoomGUI.java
+javac -sourcepath src -d bin src/ChatGUI.java
+jar -cfm BBChat.jar MainClass resources -C bin/ .
