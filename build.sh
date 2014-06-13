35c9556bbf74786b7c70baa0671d62ff6dfd4c30
ekongg <elk2son@yahoo.com>
2014-06-13 00:04:45 -0400

    Fuck

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -f *.class *.jar
 javac -sourcepath src -d bin src/BriBorChat.java
-jar -cfm BBChat.jar MainClass resources -C bin/ .
+jar -cfm BriBorChat.jar MainClass resources -C bin/ .
